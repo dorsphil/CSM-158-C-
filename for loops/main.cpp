5b4fc19e4fb65c7a8591e506661bb4cd9f6adbe6
dorsphil <dorsphilloseiasuming@gmail.com>
2024-07-19 16:04:45 +0000

    Add files via upload

---
+++ for loops/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    for(i=0; i<=10; i=i+2){
+    cout << "What matters is who you've become" << endl;
+    }
+    return 0;
+}
